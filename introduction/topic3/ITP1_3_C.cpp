44f119fb4a89bd875fc618e510c58a70066b0d21
tori-takashi <tori.toutoshi@gmail.com>
2023-07-08 04:17:34 +0900

    ITP1_3_C

---
+++ introduction/topic3/ITP1_3_C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    while (true)
+    {
+        int x, y;
+        std::cin >> x >> y;
+        if (x == 0 && y == 0)
+            break;
+
+        if (x <= y)
+        {
+            std::cout << x << " " << y << std::endl;
+        }
+        else
+        {
+            std::cout << y << " " << x << std::endl;
+        }
+    }
+    return 0;
+}
